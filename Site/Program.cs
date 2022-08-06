c61812141ad3bb6971d37ca65fff8520df8d2a4e
Alberto Malavassi Viales <malavassiv@gmail.com>
2022-08-06 15:37:51 -0600

    Initial Skeleton

---
+++ Site/Program.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Site;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        await builder.Build().RunAsync();
+    }
+}
